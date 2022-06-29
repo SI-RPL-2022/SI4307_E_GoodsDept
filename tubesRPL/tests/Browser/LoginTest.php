288f94da1f6ed5c26cd85269ed1f4123e568b0f5
pradanavega <pradanavega@student.telkomuniversity.ac.id>
2022-06-30 02:49:45 +0700

    Login Logout Test

---
+++ tubesRPL/tests/Browser/LoginTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Dashboard')
+                    ->click('a','LOGIN')
+                    ->visit('/login')
+                    ->assertSee('Email')
+                    ->type('email', 'admin@gmail.com')
+                    ->type('password', 'admin123')
+                    ->press('Login')
+                    ->visit('/homeAdmin');
+        });
+    }
+}
---
+++ tubesRPL/tests/Browser/LogoutTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LogoutTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Dashboard')
+                    ->click('a','LOGIN')
+                    ->visit('/login')
+                    ->assertSee('Email')
+                    ->type('email', 'admin@gmail.com')
+                    ->type('password', 'admin123')
+                    ->press('Login')
+                    ->visit('/homeAdmin')
+                    ->assertSee('Dashboard')
+                    ->click('a','LOGOUT');
+        });
+    }
+}
