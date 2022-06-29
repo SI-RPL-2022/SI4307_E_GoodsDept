2b362d2e44ddca840ae60722af90139b58607e84
YonathanDaniel <nathan@student.telkomuniversity.ac.id>
2022-06-29 22:15:52 +0700

    Profile

--- tubesRPL/resources/views/profile.blade.php
+++ tubesRPL/resources/views/profile.blade.php
@@ -3,37 +3,84 @@
 @section('title','Profile') 
 
 @section('content')
-<div class="container" style="margin:20px auto 50px auto">
-    <h1 class="text-center">Profile</h1>
+<section style="background-color: #eee;">
+  <div class="container py-5">
     <div class="row">
-      <div class="card mb-3">
-        <div class="card-body">
-          <table>
-            <tr>
-              <td><h5 class="card-title">Nama User</h5></td>
-              <td>:</td>
-              <td><h5 class="card-title">{{ auth()->user()->nama_user }}</h5></td>
-            </tr>
-            <tr>
-              <td><p class="card-text">Email</p></td>
-              <td>:</td>
-              <td><p class="card-text">{{ auth()->user()->email }}</p></td>
-            </tr>
-            <tr>
-              <td><p class="card-text">Alamat</p></td>
-              <td>:</td>
-              <td><p class="card-text">{{ auth()->user()->alamat }}</p></td>
-            </tr>
-            <tr>
-              <td><p class="card-text">No. Telp</p></td>
-              <td>:</td>
-              <td><p class="card-text">{{ auth()->user()->no_tlp }}</p></td>
-            </tr>
-          </table>
-        </div>
+      <div class="col">
+        <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
+          <ol class="breadcrumb mb-0">
+            <li class="breadcrumb-item">PROFILE</a></li>
+          </ol>
+        </nav>
+      </div>
     </div>
-    
+
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="card mb-4">
+          <div class="card-body text-center">
+            <img src="https://cdn-icons.flaticon.com/png/512/552/premium/552721.png?token=exp=1656515950~hmac=faddfe8db557f755d03b4795a95711ee" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 150px;">
+            <h5 class="my-3">{{ auth()->user()->nama_user }}</h5>
+          
+            @auth
+            @if (auth()->user()->level == 1)
+            <p class="text-muted mb-1">ADMIN</p>
+          
+            @endif   
+
+            @if (auth()->user()->level == 2)
+            <p class="text-muted mb-1">KASIR</p>
+
+            @endif 
+        @endauth
+            <p class="text-muted mb-4">{{ auth()->user()->alamat }}</p>
+            <div class="d-flex justify-content-center mb-2">
+            </div>
+          </div>
+        </div>
+        <div class="card mb-4 mb-lg-0">
+          <div class="card-body p-0">
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-8">
+        <div class="card mb-4">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Email</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ auth()->user()->email }}</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Phone</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ auth()->user()->no_tlp }}</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Address</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ auth()->user()->alamat }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        </div>
+      </div>
+    </div>
   </div>
-</div>
+</section>
 
 @endsection
