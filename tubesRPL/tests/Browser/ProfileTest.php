84d78f8d298a5efe8d8bde9e1aad1c0a95c25252
YonathanDaniel <nathan@student.telkomuniversity.ac.id>
2022-06-30 12:09:39 +0700

    Profile Test

---
+++ tubesRPL/tests/Browser/ProfileTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ProfileTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Dashboard')
+                    ->click('a','LOGIN')
+                    ->visit('/login')
+                    ->assertSee('Email')
+                    ->type('email', 'admin@gmail.com')
+                    ->type('password', 'admin123')
+                    ->press('Login')
+                    ->visit('/homeAdmin')
+                    ->assertSee('Selamat Datang')
+                    ->press('a','PROFIL')
+                    ->visit('/profile');
+        });
+    }
+}
